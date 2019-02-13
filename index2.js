972ad675abbb3fdf964d7d52766d2a2f14c1f109
Kudzanayi Dzvairo <kudzanayi@Kudzanayis-MacBook-Air.local>
2019-02-13 17:04:24 -0500

    sidebar

--- index2.js
+++ index2.js
@@ -1,17 +1,33 @@
 document.addEventListener("DOMContentLoaded", e => {
 
-const sideBarDiv = document.createElement('div')
-const postUl = document.createElement('ul')
-postUl.className = 'post-ul'
-sideBarDiv.append(postUl)
 
-console.log(postUl)
+
 
 fetch('http://localhost:3000/api/v1/posts')
 .then(res => res.json())
 .then(post => {
-	console.log
+	post.forEach(sideBar)
 })
+
+const postUl = document.querySelector('#note-list')
+
+function sideBar (posts){
+
+	postUl.innerHTML = `<li data-id=${posts.id} class='post'>${posts.title}</li>`
+
+}
+
+postUl.addEventListener('click', showPost)
+
+function showPost(e){
+  if(e.target.className === 'post'){
+		
+
+	}
+
+}
+
+
 
 
 
