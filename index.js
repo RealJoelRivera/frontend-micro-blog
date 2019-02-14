fcc3c3096f703ae1dc819ef6a670ff691119df7d
woodylucas <wlucas0311@gmail.com>
2019-02-14 15:12:30 -0500

    added frontend style

--- index.js
+++ index.js
@@ -4,10 +4,43 @@ const url = 'http://localhost:3000/api/v1/users'
 
 const microData = await data()
 
-const commentDiv = document.querySelector('#comment-div')
+const formContainer = document.querySelector('#form-container')
+addPost = false;
+const usersName = document.querySelector('#usersname')
+
+const postList = document.querySelector('#post-list')
+
+const mainPost = document.querySelector('#main-post');
+
+const postForm = document.querySelector('#post-form')
+const newBtn = document.querySelector('#new-post-btn')
 
 
-function appHTML(app) {
+newBtn.addEventListener('click', () => {
+  // hide & seek with the form
+  addPost = !addPost
+  if (addPost) {
+    postForm.style.display = 'block'
+    // submit listener here
+  } else {
+    postForm.style.display = 'none'
+  }
+})
+
+
+
+const renderShowPage = (blog) => {
+  blog.forEach(showPage)
+}
+
+
+
+
+function showPage(blog) {
+  // debugger
+  blog.posts.forEach(post => {
+    postList.innerHTML = `<li data-id=${post.id}>${post.title}</li>`
+  });
 }
 
 
@@ -18,11 +51,10 @@ async function data() {
 }
 
 
+console.log(microData)
 
 
-
-
-
+renderShowPage(microData);
 
 
  });
